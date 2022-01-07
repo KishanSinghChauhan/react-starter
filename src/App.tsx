6450c5efa4ad56c3af5deceb1c2eb3f63730babb
Kishan Singh <kishan.singh@squareboat.com>
2022-01-07 15:14:18 +0530

    Add: hot reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import "./styles.css";
+import Image from "./assets/images.png";
+
 export const App = () => {
-  return <h1>React Starter</h1>;
+  return (
+    <>
+      <h1>React Starter {process.env.name}</h1>
+      <img src={Image} />
+    </>
+  );
 };
